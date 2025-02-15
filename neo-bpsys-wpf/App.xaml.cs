574dc1a34044d0f67b3c02f7bb5ef9fabc8f3f2d
gxpjy <gxpjy@ZERO_PLFJY>
2025-02-15 23:35:57 +0800

    添加项目文件。

---
+++ neo-bpsys-wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace neo_bpsys_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
