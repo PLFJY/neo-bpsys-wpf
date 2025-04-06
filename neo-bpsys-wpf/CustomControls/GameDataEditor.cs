299b4ced8d92948e9c3af859169e7da5130b0fb7
零风PLFJY <gxpjy2008@163.com>
2025-04-06 13:59:49 +0800

    后台UI完成，添加版本号自动跟进，添加安装程序生成部分的脚本，采用inno setup
    (cherry picked from commit 0484712644bd5084eb07c7334efdb0302c3e4a8c)

---
+++ neo-bpsys-wpf/CustomControls/GameDataEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace neo_bpsys_wpf.CustomControls
+{
+    public class GameDataEditor : Control
+    {
+        public string GameDataValue
+        {
+            get { return (string)GetValue(GameDataValueProperty); }
+            set { SetValue(GameDataValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GameDataValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GameDataValueProperty =
+            DependencyProperty.Register("GameDataValue", typeof(string), typeof(GameDataEditor), new PropertyMetadata(null));
+
+        public string GameDataType
+        {
+            get { return (string)GetValue(GameDataTypeProperty); }
+            set { SetValue(GameDataTypeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GameDataType.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GameDataTypeProperty =
+            DependencyProperty.Register("GameDataType", typeof(string), typeof(GameDataEditor), new PropertyMetadata(null));
+    }
+}
