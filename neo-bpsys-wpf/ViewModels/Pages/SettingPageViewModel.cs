7a105439e9ca7232a4ef20e7d29d77ec37e487db
PLFJY <gxpjy2008@163.com>
2025-05-09 00:09:58 +0800

    完成了地图BP的设计者模式
    (cherry picked from commit 94876d5f2c1ba1df04547e0a178e89a124a2b39f)
    (cherry picked from commit 35a5af8009f0a27c071976e73f0933346413b119)

--- neo-bpsys-wpf/ViewModels/Pages/SettingPageViewModel.cs
+++ neo-bpsys-wpf/ViewModels/Pages/SettingPageViewModel.cs
@@ -7,7 +7,7 @@ namespace neo_bpsys_wpf.ViewModels.Pages
         public SettingPageViewModel()
         {
             //Decorative constructor, used in conjunction with IsDesignTimeCreatable=True
-            AppVersion = "版本 " + App.ResourceAssembly.GetName().Version!.ToString();
+            AppVersion = "版本 v" + App.ResourceAssembly.GetName().Version!.ToString();
         }
 
         [ObservableProperty]
