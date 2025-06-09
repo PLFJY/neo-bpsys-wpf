e3dda1c13a108e6bf5d0b317f0dd3ebcb352b1e8
零风PLFJY <gxpjy2008@163.com>
2025-06-09 20:45:32 +0800

    fix<InterludeWindow>: 修复了角色立绘会Overflow的问题
    fix<GameDataPage, TalentPage>:
    修复了绑定的CharacterName方式错误的问题

--- neo-bpsys-wpf/ViewModels/Pages/TalentPageViewModel.cs
+++ neo-bpsys-wpf/ViewModels/Pages/TalentPageViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using neo_bpsys_wpf.Messages;
+using neo_bpsys_wpf.Models;
 using neo_bpsys_wpf.Services;
 
 namespace neo_bpsys_wpf.ViewModels.Pages
 {
-    public partial class TalentPageViewModel : ObservableObject
+    public partial class TalentPageViewModel : ObservableRecipient, IRecipient<NewGameMessage>, IRecipient<PropertyChangedMessage<bool>>
     {
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
         public TalentPageViewModel()
@@ -12,11 +16,12 @@ namespace neo_bpsys_wpf.ViewModels.Pages
             //Decorative constructor, used in conjunction with IsDesignTimeCreatable=True
         }
 
-        public ISharedDataService SharedDataService { get; }
+        public ISharedDataService _sharedDataService { get; }
 
         public TalentPageViewModel(ISharedDataService sharedDataService)
         {
-            SharedDataService = sharedDataService;
+            _sharedDataService = sharedDataService;
+            IsActive = true;
         }
 
         private Enums.Trait? _selectedTrait = null;
@@ -27,7 +32,38 @@ namespace neo_bpsys_wpf.ViewModels.Pages
             set
             {
                 _selectedTrait = value;
-                SharedDataService.CurrentGame.HunPlayer.Trait = new(_selectedTrait);
+                _sharedDataService.CurrentGame.HunPlayer.Trait = new(_selectedTrait);
+                OnPropertyChanged();
+            }
+        }
+
+        public Game CurrentGame => _sharedDataService.CurrentGame;
+
+        public void Receive(NewGameMessage message)
+        {
+            if (message.IsNewGameCreated)
+            {
+                OnPropertyChanged(nameof(CurrentGame));
+            }
+        }
+
+        public void Receive(PropertyChangedMessage<bool> message)
+        {
+            if (message.PropertyName == nameof(ISharedDataService.IsTraitVisible))
+            {
+                IsTraitVisible = message.NewValue;
+            }
+        }
+
+        private bool _isTraitVisible = true;
+
+        public bool IsTraitVisible
+        {
+            get { return _isTraitVisible; }
+            set
+            {
+                _isTraitVisible = value;
+                _sharedDataService.IsTraitVisible = _isTraitVisible;
                 OnPropertyChanged();
             }
         }
