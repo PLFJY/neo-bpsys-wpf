01a21b1f32b269722a28495be2e3c4f6b96561c9
ZackZhao233 <wszwbzackzs666@126.com>
2025-06-12 20:43:23 +0800

    chore<PlayerData>: 修改了GameData的数据类型和初始值（从double修改为string）

--- neo-bpsys-wpf/Models/PlayerData.cs
+++ neo-bpsys-wpf/Models/PlayerData.cs
@@ -8,33 +8,33 @@ public partial class PlayerData : ObservableObject
 {
     // Sur 相关数据
     [ObservableProperty]
-    private double _machineDecoded = 0; // 破译进度
+    private string _machineDecoded = string.Empty; // 破译进度
 
     [ObservableProperty]
-    private double _palletStunTimes = 0; // 砸板命中次数
+    private string _palletStunTimes = string.Empty; // 砸板命中次数
 
     [ObservableProperty]
-    private double _rescueTimes = 0; // 救人次数
+    private string _rescueTimes = string.Empty; // 救人次数
 
     [ObservableProperty]
-    private double _healedTimes = 0; // 治疗次数
+    private string _healedTimes = string.Empty; // 治疗次数
 
     [ObservableProperty]
-    private double _kiteTime = 0; // 牵制时间
+    private string _kiteTime = string.Empty; // 牵制时间
 
     // Hun 相关数据
     [ObservableProperty]
-    private double _machineLeft = 0; // 剩余密码机数量
+    private string _machineLeft = string.Empty; // 剩余密码机数量
 
     [ObservableProperty]
-    private double _palletBroken = 0; // 破坏板子数
+    private string _palletBroken = string.Empty; // 破坏板子数
 
     [ObservableProperty]
-    private double _hitTimes = 0; // 命中求生者次数
+    private string _hitTimes = string.Empty; // 命中求生者次数
 
     [ObservableProperty]
-    private double _terrorShockTimes = 0; // 恐惧震慑次数
+    private string _terrorShockTimes = string.Empty; // 恐惧震慑次数
 
     [ObservableProperty]
-    private double _downTimes = 0; // 击倒次数
+    private string _downTimes = string.Empty; // 击倒次数
 }
