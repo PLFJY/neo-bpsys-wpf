4290a321ff457acfb8d28ca172112c0a9d675e30
Tianqi <tian_qi_awa@163.com>
2025-07-31 02:10:37 +0800

    feat: Extensions: Add Extension Interfaces

--- neo-bpsys-wpf.Core/Extensions/IExtension.cs
+++ neo-bpsys-wpf.Core/Extensions/IExtension.cs
@@ -2,5 +2,20 @@ namespace neo_bpsys_wpf.Core.Extensions;
 
 public interface IExtension
 {
-    
+    /// <summary>
+    /// 插件信息
+    /// </summary>
+    public ExtensionInfo ExtensionInfo { get; internal set; }
+    /// <summary>
+    /// 当插件被注册时调用，用于初始化插件的相关资源或状态。
+    /// </summary>
+    public void Initialize();
+    /// <summary>
+    /// 当插件启用时调用
+    /// </summary>
+    public void OnEnable();
+    /// <summary>
+    /// 当插件禁用时调用
+    /// </summary>
+    public void OnDisable();
 }
