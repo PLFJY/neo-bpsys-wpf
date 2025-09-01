77aa509df64d8042db00676aca7dd6a933e66f0c
PLFJY <gxpjy2008@163.com>
2025-09-01 20:33:24 +0800

    refactor (SnackBar): 规范了一下命名，然后给ISnackbarService加了下注释，修改了颜文字，分离了HyperLinkSnackbarContent的text设置，跳转Uri换为程序集路径

--- neo-bpsys-wpf/Services/SnackbarService.cs
+++ neo-bpsys-wpf/Services/SnackbarService.cs
@@ -10,22 +10,34 @@ public class SnackbarService : ISnackbarService
     private SnackbarPresenter? _presenter;
     private Snackbar? _snackbar;
 
-    /// <inheritdoc />
+    /// <summary>
+    /// 默认关闭时间
+    /// </summary>
     public TimeSpan DefaultTimeOut { get; set; } = TimeSpan.FromSeconds(5L);
 
-    /// <inheritdoc />
+    /// <summary>
+    /// 设置<see cref="SnackbarPresenter"/>控件
+    /// </summary>
+    /// <param name="contentPresenter"><see cref="SnackbarPresenter"/>控件</param>
     public void SetSnackbarPresenter(SnackbarPresenter contentPresenter)
     {
         this._presenter = contentPresenter;
+
     }
 
-    /// <inheritdoc />
-    public SnackbarPresenter? GetSnackbarPresenter() => this._presenter;
-
-    /// <inheritdoc />
+    /// <summary>
+    /// 显示
+    /// </summary>
+    /// <param name="title">标题</param>
+    /// <param name="content">显示内容</param>
+    /// <param name="appearance"><see cref="SnackbarPresenter"/>类型</param>
+    /// <param name="icon">图标</param>
+    /// <param name="timeout">超时自动隐藏时间</param>
+    /// <param name="isCloseButtonEnabled">关闭按钮是否可用</param>
+    /// <exception cref="InvalidOperationException">未设置SnackbarControl</exception>
     public void Show(
         string title,
-        DependencyObject message,
+        DependencyObject content,
         ControlAppearance appearance,
         IconElement? icon,
         TimeSpan timeout,
@@ -33,14 +45,14 @@ public class SnackbarService : ISnackbarService
     {
         if (this._presenter == null)
             throw new InvalidOperationException("The SnackbarPresenter was never set");
-        if (this._snackbar == null)
-            this._snackbar = new Snackbar(this._presenter);
-        this._snackbar.SetCurrentValue(Snackbar.TitleProperty, (object) title);
-        this._snackbar.SetCurrentValue(ContentControl.ContentProperty, (object) message);
-        this._snackbar.SetCurrentValue(Snackbar.AppearanceProperty, (object) appearance);
-        this._snackbar.SetCurrentValue(Snackbar.IconProperty, (object) icon);
-        this._snackbar.SetCurrentValue(Snackbar.TimeoutProperty, (object) (timeout.TotalSeconds == 0.0 ? this.DefaultTimeOut : timeout));
-        this._snackbar.SetCurrentValue(Snackbar.IsCloseButtonEnabledProperty, (object) isCloseButtonEnabled);
+        this._snackbar ??= new Snackbar(this._presenter);
+        this._snackbar.SetCurrentValue(Snackbar.TitleProperty, (object)title);
+        this._snackbar.SetCurrentValue(ContentControl.ContentProperty, (object)content);
+        this._snackbar.SetCurrentValue(Snackbar.AppearanceProperty, (object)appearance);
+        if(icon != null)
+            this._snackbar.SetCurrentValue(Snackbar.IconProperty, (object)icon);
+        this._snackbar.SetCurrentValue(Snackbar.TimeoutProperty, (object)(timeout.TotalSeconds == 0.0 ? this.DefaultTimeOut : timeout));
+        this._snackbar.SetCurrentValue(Snackbar.IsCloseButtonEnabledProperty, (object)isCloseButtonEnabled);
         this._snackbar.Show(true);
     }
 }
