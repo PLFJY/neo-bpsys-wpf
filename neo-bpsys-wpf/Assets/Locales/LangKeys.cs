1c53614eef8049cc05cf623f8106e968e301b3dc
ZackZhao233 <wszwbzackzs666@126.com>
2025-09-23 22:35:00 +0800

    feat: 完成了i18n的一部分

---
+++ neo-bpsys-wpf/Assets/Locales/LangKeys.cs
@@ -0,0 +1,11 @@
+﻿using Antelcat.I18N.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neo_bpsys_wpf.Assets.Locales;
+
+[ResourceKeysOf(typeof(Lang))]
+public partial class LangKeys;
